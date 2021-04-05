ddd5e2d65b7aeaef45189f1c5c071c544568497d
Lucas Miranda <lucasalternativo.lg@gmail.com>
2021-04-05 17:12:47 -0300

    Initial commit

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoalA: "Finish the course and learn Vue!",
+            courseGoalB: "Master Vue and build amazing apps!",
+            htmlContent: "<h1>Get in the robot, Shinji</h1>",
+            vueLink: "https://vuejs.org"
+        };
+    },
+
+    methods: {
+        outputGoal(){
+            const randomNumber = Math.random();
+
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        } 
+    }
+});
+
+app.mount("#user-goal");
