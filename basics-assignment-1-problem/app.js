f46a890bcf7123074c3ecb46a8072e420d6e1490
Lucas Miranda <lucasalternativo.lg@gmail.com>
2021-04-05 17:40:42 -0300

    Add asignment 1

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      age: 21,
+      name: "Lucas G. M. Miranda",
+      imgUrl:
+        "https://media-exp1.licdn.com/dms/image/C4E03AQHqVIcgkoHcMQ/profile-displayphoto-shrink_200_200/0/1610555507408?e=1620864000&v=beta&t=SsyINm7xQx0eQ01yPCMp21YOlbHmrolKqr8RNoffkdM",
+    };
+  },
+});
+
+app.mount("#assignment");
