69e44fc121e318a89a0fbe13dea85a1290fb896e
Lucas Miranda <lucasalternativo.lg@gmail.com>
2021-07-23 18:31:42 -0300

    Solved components exercise

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <table>
+    <tr>
+      <td class="active-user-cell">
+        <active-user :username="username" :age="age"></active-user>
+      </td>
+      <td class="user-data-cell">
+        <user-data @update-active-user-data="updateActiveUserData"></user-data>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+import ActiveUser from "./components/ActiveUser.vue";
+export default {
+  components: { ActiveUser },
+  data() {
+    return {
+      username: "",
+      age: "",
+    };
+  },
+  methods: {
+    updateActiveUserData(activeUserData) {
+      this.username = activeUserData.username;
+      this.age = activeUserData.age;
+    },
+  },
+};
+</script>
+
+<style>
+.user-data-cell {
+  padding-left: 100px;
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="updateActiveUserData">
+    <label>Username:</label>
+    <input v-model="username" type="text" />
+    <label>Age:</label>
+    <input v-model="age" type="number" />
+    <button>Submit</button>
+  </form>
+</template>
+<script>
+export default {
+  emits: {
+    "update-active-user-data": (activeUserData) => {
+      if (typeof activeUserData === "object") {
+        return true;
+      } else {
+        console.warn("'activeUserData' must be an object!");
+        return false;
+      }
+    },
+  },
+  data() {
+    return {
+      username: "",
+      age: "",
+    };
+  },
+  methods: {
+    updateActiveUserData() {
+      this.$emit("update-active-user-data", {
+        username: this.username,
+        age: this.age,
+      });
+    },
+  },
+};
+</script>
